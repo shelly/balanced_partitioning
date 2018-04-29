32c830a91368afb92f403aef490f1148058bcf59
Raunak (aware) <raunakagnihotri@github.com>
2018-04-29 13:18:14 -0400

    Similarity function written and compiles.

--- ligra-master/apps/BalancedPartitioning.C
+++ ligra-master/apps/BalancedPartitioning.C
@@ -1,10 +1,47 @@
 #include "ligra.h"
 #include <stdio.h>
 
+intE *listToIndicator(intE *vertices, intE vertices_size, long n) {
+    int *indicator = newA(int, n);
+    parallel_for(long i = 0; i < vertices_size; i++) {
+        indicator[vertices[i]] = 1;
+    } 
+    return indicator;
+}
+
+
+template <class vertex>
+int *Similarity(graph<vertex>& G) {
+    long n = G.n;
+    int *similarity = newA(int, n * n);
+    parallel_for(long i = 0; i < n; i++) {
+        vertex v = G.V[i];
+        intE v_degree = v.getOutDegree();
+	intE *v_neighbors = v.getOutNeighbors();
+        parallel_for(long j = 0; j < v_degree; j ++) {
+	    vertex u = v_neighbors[j];
+            intE u_degree = u.getOutDegree();
+	    intE *u_neighbors = u.getOutNeighbors();	    
+
+            intE *v_ind = listToIndicator(v_neighbors, v_degree, n);
+	    intE *u_ind = listToIndicator(u_neighbors, u_degree, n);            
+            intE *intersection = newA(int, n);
+
+            parallel_for(long k = 0; k < n; k++) {
+                 intersection[k] = v_ind[k] * u_ind[k];
+            }
+
+            similarity[n*v + u] = sequence::plusReduce(intersection, n); 
+
+        }
+    }
+    return similarity;
+}
+
+
 template <class vertex>
 void Compute(graph<vertex>& G, commandLine P) {
-	long n = G.n;
-	long m = G.m;
-	printf("Number of vertices in G: %li\n", n);
-	printf("Number of edges in G: %li\n", m);	
+
+    //int *similarity = Similarity(G);
+    printf("Completed successfully.\n");
 }
